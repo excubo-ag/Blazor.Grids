88fdab935a8646fd327934a7e6568f5d0cf387bd
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-07-09 15:40:35 +0200

    removed useless resources

--- Grid/wwwroot/script.js
+++
@@ -1,5 +0,0 @@
-window.Excubo = window.Excubo || {};
-window.Excubo.Grids = {
-    GetPosition: (el) => [el.offsetLeft, el.offsetTop],
-    GetDimensions: (el) => [el.clientWidth, el.clientHeight]
-};
