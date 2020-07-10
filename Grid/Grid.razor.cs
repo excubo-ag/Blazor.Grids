fb74f5e671a752eb21503a12cae6cf0c3f20fbba
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-07-10 16:52:19 +0200

    minified style

--- Grid/Grid.razor.cs
+++ Grid/Grid.razor.cs
@@ -119,7 +119,11 @@ namespace Excubo.Blazor.Grids
         {
             if (firstRender)
             {
+#if DEBUG
                 const string src = "_content/Excubo.Blazor.Grids/style.css";
+#else
+                const string src = "_content/Excubo.Blazor.Grids/style.min.css";
+#endif
                 var condition = $"document.head.querySelector(`[src='{src}']`) == null";
                 var action = $"let s = document.createElement('link'); s.setAttribute('rel', 'stylesheet'); s.setAttribute('href', '{src}'); document.head.appendChild(s);";
                 await js.InvokeVoidAsync("eval", $"if ({condition}) {{ {action} }}");
